ef4101fd36de23fc4b0f838a965b05b9276925c0
Tauan Binato <tauanflores@gmail.com>
2016-11-09 01:26:40 -0200

    Initial Commit

---
+++ BullNCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullNCowGame
+//
+//  Created by Tauan Binato Flores on 09/11/16.
+//  Copyright © 2016 Tauan Binato Flores. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
